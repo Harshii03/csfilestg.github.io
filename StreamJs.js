801f7960340af7b13fd0bf2358ea080bc1c9b395
Biisal <140535337+Aeditx03@users.noreply.github.com>
2023-11-24 18:37:39 +0530

    Update StreamJs.js

--- StreamJs.js
+++ StreamJs.js
@@ -3,61 +3,76 @@ let abtBtn = document.querySelector(".about-btn")
 let dldBtn_outer = document.querySelector(".downloadBtn")
 let file_name = document.querySelector(".file-name")
 let about_nav = document.querySelector(".about-nav")
+let bot_btn = document.querySelector('.bot-btn')
+let contact_btn = document.querySelector('.contact-btn')
 let links = document.querySelectorAll('.links a')
+let bot_links = document.querySelectorAll('.bot-link a')
+let chnl_link = document.querySelectorAll('.chnl-link a')
 let abt_chnl = document.querySelector('.abt-chnl')
+let contact = document.querySelectorAll('.contact a')
 let timer = 0
 // adding under in home btn at first 
 homeBtn.classList.add('active');
-
-
-
 // when clicking about
 abtBtn.addEventListener("click", () => {
     dldBtn_outer.style.display = "none";
     file_name.style.display = "none";
     about_nav.style.display = "block"
-
-
+    about_nav.style.display = "block"
+    about_nav.style.animation = "strtLoad 1s ease 0s forwards"
 })
 // when clicking home
 homeBtn.addEventListener("click", () => {
     dldBtn_outer.style.display = "flex";
     file_name.style.display = "block";
     window.location.href = "#main";
+    about_nav.style.animation = "strtLoad 1s ease 0s forwards"
     about_nav.style.display = "none"
 
 })
+abt_chnl.addEventListener("click", () => {
+    timer = 1
+    chnl_link.forEach((i) => {
+        i.style.animation = `strtLoad 1s ease ${timer}s forwards, linksBtnAn 2s ease ${timer}s infinite `
+        timer += 0.3;
+    });
+    timer = 0
+});
 
+bot_btn.addEventListener("click", () => {
+    timer = 1;
+    bot_links.forEach((i) => {
+        i.style.animation = `linksBtnAn 2s ease ${timer}s infinite ,strtLoad 1s ease ${timer}s forwards`;
+        timer += 0.3;
+    });
+
+    timer = 0;
+})
+contact_btn.addEventListener("click", () => {
+    timer = 1;
+    contact.forEach((i) => {
+        i.style.animation = `linksBtnAn 2s ease ${timer}s infinite ,strtLoad 1s ease ${timer}s forwards`;
+        timer += 0.3;
+    });
+
+    timer = 0;
+})
 
 // btn animations 
 let dldBtn = document.querySelectorAll('.downloadBtn button')
 
 timer = 0
 links.forEach((i) => {
-    i.style.animation = `linksBtnAn 2s ease ${timer}s infinite `
+    i.style.animation = `linksBtnAn 2s ease ${timer}s infinite`
 
     timer += 0.3
 })
+timer = 0
 dldBtn.forEach((i) => {
     i.style.animation = `strtLoad 1s ease ${timer}s forwards, linksBtnAn 2s ease ${timer}s infinite `
     timer += 0.3
 })
 timer = 0
-
-abt_chnl.addEventListener("click", () => {
-    console.log(timer, " hii");
-    links.forEach((i) => {
-        i.style.animation = `linksBtnAn 2s ease ${timer}s infinite , strtLoad 1s ease ${timer}s forwards`
-
-        timer += 0.3
-    })
-    timer = 0
-})
-
-
-
-// ################################
-
 function toggleWidth(element) {
     // Remove 'active' class from all links
     var links = document.querySelectorAll('.about-nav a');
@@ -83,29 +98,15 @@ function toggleWidthnav(element) {
     });
     let wlcm = document.querySelector(".wlcm")
     wlcm.classList.add('active');
-    console.log(sec1);
 }
-
-
-// #####################################
-
-Shery.mouseFollower();
-Shery.makeMagnet(".magnet");
-
-// #####################################
-
-
-
-// #####################################
+// Shery.mouseFollower();
+// Shery.makeMagnet(".magnet");
 // file name showing 
 var div = document.getElementById('myDiv');
 var text = div.textContent;
-
 if (text.length > 300) {
     div.textContent = text.slice(0, 300) + "....";
 }
-
-
 // video player 
 const controls = [
     'play-large',
@@ -114,8 +115,8 @@ const controls = [
     'progress',
     'current-time',
     'duration',
-    'mute',
-    'volume',
+    // 'mute',
+    // 'volume',
     'captions',
     'settings',
     'pip',
@@ -127,13 +128,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const player = Plyr.setup('.player', { controls });
 });
 
-
-
-
-// < disabling right click
-document.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
-});
+// disabling right click
+    document.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+    });
 document.addEventListener('keydown', function (e) {
     if (
         e.key === 'F12' ||
@@ -147,49 +145,46 @@ document.addEventListener('keydown', function (e) {
     }
 });
 
-
-
 const videolink = window.location.href;
-const lazyLink = videolink.replace("/watch/", "/");
+const bisallink = videolink.replace("/watch/", "/");
 
 function vlc_player() {
-    const openLazyLink = lazyLink;
-    const openVlc = `vlc://${openLazyLink}`;
+    const openbisallink = bisallink;
+    const openVlc = `vlc://${openbisallink}`;
     window.location.href = openVlc;
 }
 
 function mx_player() {
-    const openLazyLink = lazyLink;
-    const openMx = `intent:${openLazyLink}#Intent;package=com.mxtech.videoplayer.ad;end`;
+    const openbisallink = bisallink;
+    const openMx = `intent:${openbisallink}#Intent;package=com.mxtech.videoplayer.ad;end`;
     window.location.href = openMx;
 }
 
 function playit_player() {
-    const openLazyLink = lazyLink;
-    const openPlayit = `playit://playerv2/video?url=${openLazyLink}`;
+    const openbisallink = bisallink;
+    const openPlayit = `playit://playerv2/video?url=${openbisallink}`;
     window.location.href = openPlayit;
 }
 
 function s_player() {
-    const openLazyLink = lazyLink;
-    const openSplayer = `intent:${openLazyLink}#Intent;action=com.young.simple.player.playback_online;package=com.young.simple.player;end`;
+    const openbisallink = bisallink;
+    const openSplayer = `intent:${openbisallink}#Intent;action=com.young.simple.player.playback_online;package=com.young.simple.player;end`;
     window.location.href = openSplayer;
 }
 
 function km_player() {
-    const openLazyLink = lazyLink;
-    const openKmplayer = `intent:${openLazyLink}#Intent;package=com.kmplayer;end`;
+    const openbisallink = bisallink;
+    const openKmplayer = `intent:${openbisallink}#Intent;package=com.kmplayer;end`;
     window.location.href = openKmplayer;
 }
 
 function hd_player() {
-    const openLazyLink = lazyLink;
-    const openHDplayer = `intent:${openLazyLink}#Intent;package=uplayer.video.player;end`;
+    const openbisallink = bisallink;
+    const openHDplayer = `intent:${openbisallink}#Intent;package=uplayer.video.player;end`;
     window.location.href = openHDplayer;
 }
 
 function lazyDownload() {
-    const openLazyLink = lazyLink;
-    window.location.href = openLazyLink;
+    const openbisallink = bisallink;
+    window.location.href = openbisallink;
 }
-
