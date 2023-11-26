356837a9a670665c7584701e635677a4ed7881f3
Aeditx03 <aeditx03@gmail.com>
2023-11-26 21:47:41 +0530

    new update..more anm added

--- StreamJs.js
+++ StreamJs.js
@@ -10,29 +10,40 @@ let bot_links = document.querySelectorAll('.bot-link a')
 let chnl_link = document.querySelectorAll('.chnl-link a')
 let abt_chnl = document.querySelector('.abt-chnl')
 let contact = document.querySelectorAll('.contact a')
+let footer = document.querySelector('footer')
+
+
 let timer = 0
 
-// title of my webpage 
 document.querySelector(".title").textContent = 'BISAL FILES'
+
 // adding under in home btn at first 
 homeBtn.classList.add('active');
+
 // when clicking about
 abtBtn.addEventListener("click", () => {
     dldBtn_outer.style.display = "none";
     file_name.style.display = "none";
+    footer.style.display = "none";
     about_nav.style.display = "block"
     about_nav.style.display = "block"
     about_nav.style.animation = "strtLoad 1s ease 0s forwards"
+
+
+
 })
 // when clicking home
 homeBtn.addEventListener("click", () => {
     dldBtn_outer.style.display = "flex";
     file_name.style.display = "block";
+    footer.style.display = "block";
     window.location.href = "#main";
     about_nav.style.animation = "strtLoad 1s ease 0s forwards"
     about_nav.style.display = "none"
 
 })
+
+
 abt_chnl.addEventListener("click", () => {
     timer = 1
     chnl_link.forEach((i) => {
@@ -41,56 +52,119 @@ abt_chnl.addEventListener("click", () => {
     });
     timer = 0
 });
-
-bot_btn.addEventListener("click", () => {
+function bot_btn_clicked() {
+    var about_btn = document.querySelector(".about-btn")
     timer = 1;
     bot_links.forEach((i) => {
         i.style.animation = `linksBtnAn 2s ease ${timer}s infinite ,strtLoad 1s ease ${timer}s forwards`;
         timer += 0.3;
     });
     timer = 0;
-})
+    dldBtn_outer.style.display = "none";
+    file_name.style.display = "none";
+    footer.style.display = "none";
+    about_nav.style.display = "block"
+    about_nav.style.display = "block"
+    about_nav.style.animation = "strtLoad 1s ease 0s forwards"
+    var links = document.querySelectorAll('.nryt a');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    about_btn.classList.add('active');
+    var links = document.querySelectorAll('.about-nav a');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    let wlcm = document.querySelector(".wlcm")
+    wlcm.classList.add('active');
+    var links = document.querySelectorAll('.about-nav a');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    bot_btn.classList.add('active');
+};
+footer_bisal_btn_clicked = () => {
+    timer = 1;
+    contact.forEach((i) => {
+        i.style.animation = `linksBtnAn 2s ease ${timer}s infinite ,strtLoad 1s ease ${timer}s forwards`;
+        timer += 0.3;
+    });
+
+    timer = 0;
+    var about_btn = document.querySelector(".about-btn")
+    timer = 1;
+    bot_links.forEach((i) => {
+        i.style.animation = `linksBtnAn 2s ease ${timer}s infinite ,strtLoad 1s ease ${timer}s forwards`;
+        timer += 0.3;
+    });
+    timer = 0;
+    dldBtn_outer.style.display = "none";
+    file_name.style.display = "none";
+    footer.style.display = "none";
+    about_nav.style.display = "block"
+    about_nav.style.display = "block"
+    about_nav.style.animation = "strtLoad 1s ease 0s forwards"
+    var links = document.querySelectorAll('.nryt a');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    about_btn.classList.add('active');
+    var links = document.querySelectorAll('.about-nav a');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    let wlcm = document.querySelector(".wlcm")
+    wlcm.classList.add('active');
+    var links = document.querySelectorAll('.about-nav a');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    contact_btn.classList.add('active');
+}
+
 contact_btn.addEventListener("click", () => {
     timer = 1;
     contact.forEach((i) => {
         i.style.animation = `linksBtnAn 2s ease ${timer}s infinite ,strtLoad 1s ease ${timer}s forwards`;
         timer += 0.3;
     });
+
     timer = 0;
 })
 
 // btn animations 
 let dldBtn = document.querySelectorAll('.downloadBtn button')
+dldBtn.forEach((i) => {
+    i.style.animation = `strtLoad 1s ease ${timer}s forwards, linksBtnAn 2s ease ${timer}s infinite`
+    timer += 0.3;
+    i.style.setProperty("--beforestyl", `button_shine ${2 + Math.random() * 7}s ease  ${Math.random() * 10}s infinite`);
+
+})
+
 timer = 0
 links.forEach((i) => {
     i.style.animation = `linksBtnAn 2s ease ${timer}s infinite`
+    timer += 0.3
+    i.style.setProperty("--beforestyl", `button_shine ${2 + Math.random() * 7}s ease ${Math.random() * 10}s infinite`);
 
-    timer += 0.3
 })
 timer = 0
-dldBtn.forEach((i) => {
-    i.style.animation = `strtLoad 1s ease ${timer}s forwards, linksBtnAn 2s ease ${timer}s infinite `
-    timer += 0.3
-})
 timer = 0
+
 function toggleWidth(element) {
-    // Remove 'active' class from all links
     var links = document.querySelectorAll('.about-nav a');
     links.forEach(function (link) {
         link.classList.remove('active');
     });
 
-    // Add 'active' class to the clicked link
     element.classList.add('active');
 }
 function toggleWidthnav(element) {
-    // Remove 'active' class from all links
     var links = document.querySelectorAll('.nryt a');
     links.forEach(function (link) {
         link.classList.remove('active');
     });
 
-    // Add 'active' class to the clicked link
     element.classList.add('active');
     var links = document.querySelectorAll('.about-nav a');
     links.forEach(function (link) {
@@ -99,14 +173,20 @@ function toggleWidthnav(element) {
     let wlcm = document.querySelector(".wlcm")
     wlcm.classList.add('active');
 }
-// Shery.mouseFollower();
-// Shery.makeMagnet(".magnet");
+
+if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+    Shery.mouseFollower();
+    Shery.makeMagnet(".magnet");
+}
+
+
 // file name showing 
 var div = document.getElementById('myDiv');
 var text = div.textContent;
 if (text.length > 300) {
     div.textContent = text.slice(0, 300) + "....";
 }
+
 // video player 
 const controls = [
     'play-large',
@@ -144,6 +224,7 @@ document.addEventListener('keydown', function (e) {
         e.preventDefault();
     }
 });
+
 
 const videolink = window.location.href;
 const bisallink = videolink.replace("/watch/", "/");
@@ -188,3 +269,4 @@ function lazyDownload() {
     const openbisallink = bisallink;
     window.location.href = openbisallink;
 }
+
